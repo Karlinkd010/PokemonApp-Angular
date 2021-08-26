9544833c7c16423d005544aa2250b3c091f97964
KarlinCruz <kcruz@ectotec.com>
2021-08-26 13:34:17 -0500

    Component PokemonDetail Modified Design

--- src/app/pokemon/pages/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon/pages/pokemon-detail/pokemon-detail.component.ts
@@ -12,44 +12,21 @@ import { PokemonService } from '../../services/pokemon.service';
 export class PokemonDetailComponent implements OnInit {
   
   poke!:any;
-  responsiveOptions:any;
   constructor(
     private actRoute:ActivatedRoute,
     private serPoke:PokemonService
-
-  ) { 
-    this.responsiveOptions = [
-      {
-          breakpoint: '1024px',
-          numVisible: 3,
-          numScroll: 3
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1,
-          numScroll: 1
-      }
-  ];
-  }
-
-  
-
+    ) {}
+    
   ngOnInit(): void {
 
     this.actRoute.params
     .pipe(
       switchMap(({id})=>this.serPoke.getPokemonName(id)),
       tap(console.log)
-    )
-    .subscribe(pais=>this.poke=pais)
+    ).subscribe(data=>this.poke=data)
   }
 
-  }
+}
 
 
 
